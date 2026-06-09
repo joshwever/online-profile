7e459b60a00a4b0dbfa34534943d51af5b333d23
Josh Wever <joshwever@gmail.com>
2026-06-08 23:11:24 -0500

    Initialize Astro profile site with resume details

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://joshwever.github.io',
+  base: '/online-profile',
+});
